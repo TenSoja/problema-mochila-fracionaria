ff4b03c8cb304cd81fe707fc44874fca30256811
Michel de Almeida Silva <tensoja@gmail.com>
2013-01-30 17:43:26 -0200

    Construtor da classe adicionado.

--- src/br/com/micheldealmeida/Item.java
+++ src/br/com/micheldealmeida/Item.java
@@ -1,9 +1,20 @@
 package br.com.micheldealmeida;
 
 public class Item {
+	/**
+	 * @param quantidade
+	 * @param valor
+	 * @param nome
+	 */
+
 	public double quantidade;
 	public double valor;
 	public String nome;
-	
+
+	public Item(double quantidade, double valor, String nome) {
+		this.quantidade = quantidade;
+		this.valor = valor;
+		this.nome = nome;
+	}
 
 }
