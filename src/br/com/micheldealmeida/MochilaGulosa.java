8fabdd8313c765f92ccbff8fd46176922175caf0
Michel de Almeida Silva <tensoja@gmail.com>
2013-01-30 18:08:47 -0200

    Adicionado construtor da classe.

--- src/br/com/micheldealmeida/MochilaGulosa.java
+++ src/br/com/micheldealmeida/MochilaGulosa.java
@@ -3,4 +3,13 @@ package br.com.micheldealmeida;
 public class MochilaGulosa {
 	
 	private Item[] itens = null;
+
+	/**
+	 * @param itens
+	 */
+	public MochilaGulosa(Item[] itens) {
+		this.itens = itens;
+	}
+	
+	
 }
