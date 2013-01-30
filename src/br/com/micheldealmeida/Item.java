df3ae0749de616185d446863cd291ee679c8706d
Michel de Almeida Silva <tensoja@gmail.com>
2013-01-30 17:44:15 -0200

    Comentários da classe corrigidos.

--- src/br/com/micheldealmeida/Item.java
+++ src/br/com/micheldealmeida/Item.java
@@ -1,16 +1,16 @@
 package br.com.micheldealmeida;
 
 public class Item {
-	/**
-	 * @param quantidade
-	 * @param valor
-	 * @param nome
-	 */
 
 	public double quantidade;
 	public double valor;
 	public String nome;
 
+	/**
+	 * @param quantidade
+	 * @param valor
+	 * @param nome
+	 */
 	public Item(double quantidade, double valor, String nome) {
 		this.quantidade = quantidade;
 		this.valor = valor;
