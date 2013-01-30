8d894dd005a2d5f14da8f9f3a33944cf7d9f5c69
Michel de Almeida Silva <tensoja@gmail.com>
2013-01-30 18:30:36 -0200

    metodo adicionaItens criado

--- src/br/com/micheldealmeida/MochilaGulosa.java
+++ src/br/com/micheldealmeida/MochilaGulosa.java
@@ -11,5 +11,34 @@ public class MochilaGulosa {
 		this.itens = itens;
 	}
 	
+	public Item[] adicionaItens(double capacidade){
+		
+		
+		
+		Item[] itensAdicionados = new Item[this.itens.length];
+    	
+    	int itemIndice = 0;
+    	// pegar itens até que a mochila esteja cheia ou não haja mais itens para colocar
+    	while(capacidade > 0 && itemIndice < this.itens.length){
+    		if(this.itens[itemIndice].quantidade <= capacidade){
+    			// se cabe inteiro, pegue
+    			capacidade -= this.itens[itemIndice].quantidade;
+    			itensAdicionados[itemIndice] = new Item(this.itens[itemIndice].nome, this.itens[itemIndice].valor, this.itens[itemIndice].quantidade);
+    		}
+    		else{
+    			// se não cabe inteiro, pegue a fração que puder
+    			itensAdicionados[itemIndice] = new Item(this.itens[itemIndice].nome, this.itens[itemIndice].valor, capacidade);
+    			capacidade = 0;
+    		}
+    		itemIndice++;
+    	}
+    	
+    	// adiciona outros itens de quantidade 0
+    	for(int i=itemIndice; i<this.itens.length; i++){
+    		itensAdicionados[i] = new Item(this.itens[i].nome, this.itens[i].valor, 0.0);
+    	}
+    	
+    	return itensAdicionados;
+	}
 	
 }
