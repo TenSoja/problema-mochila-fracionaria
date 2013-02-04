486024859241859b83a3ea4ee335593eb07f777e
Michel de Almeida Silva <tensoja@gmail.com>
2013-02-04 14:57:54 -0200

    Correção de indentação do código.

--- src/br/com/micheldealmeida/MochilaGulosa.java
+++ src/br/com/micheldealmeida/MochilaGulosa.java
@@ -3,7 +3,7 @@ package br.com.micheldealmeida;
 import java.util.Arrays;
 
 public class MochilaGulosa {
-	
+
 	private Item[] itens = null;
 
 	/**
@@ -12,39 +12,45 @@ public class MochilaGulosa {
 	public MochilaGulosa(Item[] itens) {
 		this.itens = itens;
 	}
-	
+
 	/**
 	 * @param capacidade
 	 * @return
 	 */
-	public Item[] adicionaItens(double capacidade){
-		
+	public Item[] adicionaItens(double capacidade) {
+
 		Arrays.sort(itens);
-		
+
 		Item[] itensAdicionados = new Item[this.itens.length];
-    	
-    	int itemIndice = 0;
-    	// pegar itens até que a mochila esteja cheia ou não haja mais itens para colocar
-    	while(capacidade > 0 && itemIndice < this.itens.length){
-    		if(this.itens[itemIndice].quantidade <= capacidade){
-    			// se cabe inteiro, pegue
-    			capacidade -= this.itens[itemIndice].quantidade;
-    			itensAdicionados[itemIndice] = new Item(this.itens[itemIndice].nome, this.itens[itemIndice].valor, this.itens[itemIndice].quantidade);
-    		}
-    		else{
-    			// se não cabe inteiro, pegue a fração que puder
-    			itensAdicionados[itemIndice] = new Item(this.itens[itemIndice].nome, this.itens[itemIndice].valor, capacidade);
-    			capacidade = 0;
-    		}
-    		itemIndice++;
-    	}
-    	
-    	// adiciona outros itens de quantidade 0
-    	for(int i=itemIndice; i<this.itens.length; i++){
-    		itensAdicionados[i] = new Item(this.itens[i].nome, this.itens[i].valor, 0.0);
-    	}
-    	
-    	return itensAdicionados;
+
+		int itemIndice = 0;
+		// pegar itens até que a mochila esteja cheia ou não haja mais itens
+		// para colocar
+		while (capacidade > 0 && itemIndice < this.itens.length) {
+			if (this.itens[itemIndice].quantidade <= capacidade) {
+				// se cabe inteiro, pegue
+				capacidade -= this.itens[itemIndice].quantidade;
+				itensAdicionados[itemIndice] = new Item(
+						this.itens[itemIndice].nome,
+						this.itens[itemIndice].valor,
+						this.itens[itemIndice].quantidade);
+			} else {
+				// se não cabe inteiro, pegue a fração que puder
+				itensAdicionados[itemIndice] = new Item(
+						this.itens[itemIndice].nome,
+						this.itens[itemIndice].valor, capacidade);
+				capacidade = 0;
+			}
+			itemIndice++;
+		}
+
+		// adiciona outros itens de quantidade 0
+		for (int i = itemIndice; i < this.itens.length; i++) {
+			itensAdicionados[i] = new Item(this.itens[i].nome,
+					this.itens[i].valor, 0.0);
+		}
+
+		return itensAdicionados;
 	}
-	
+
 }
