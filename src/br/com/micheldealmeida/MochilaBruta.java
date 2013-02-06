8b5610c5c4522dd55b6b71610d84c4e62f5ec084
Michel de Almeida Silva <tensoja@gmail.com>
2013-02-06 11:47:11 -0200

    Metódo de subconjuntos adicionado.
    O método de subconjuntos cria uma lista com todos os subconjuntos
    baseados na entrada.

--- src/br/com/micheldealmeida/MochilaBruta.java
+++ src/br/com/micheldealmeida/MochilaBruta.java
@@ -1,5 +1,8 @@
 package br.com.micheldealmeida;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MochilaBruta {
 
 	private Item[] itens = null;
@@ -10,13 +13,37 @@ public class MochilaBruta {
 	public MochilaBruta(Item[] itens) {
 		this.itens = itens;
 	}
-	
-	public Item[] adicionaItens(double capacidade) {
 
-		Item[] itensAdicionados = new Item[this.itens.length];
+	public List<Item[]> subconjuntos(Item[] itens2) {
 
-		return itensAdicionados;
+		int qtdSubconjuntos = 1 << itens2.length;
+		int indiceLista = 0;
 
+		List<Item[]> listaSubconjuntos = new ArrayList<Item[]>(qtdSubconjuntos);
+
+		for (int i = 0; i < qtdSubconjuntos; i++) {
+			int pos = itens2.length - 1;
+			int bitmask = i;
+			Item[] listaAux = new Item[itens2.length];
+			while (bitmask > 0) {
+				if ((bitmask & 1) == 1) {
+
+					// System.out.print(itens2[pos].nome + ",");
+
+					listaAux[indiceLista] = new Item(itens2[pos].nome,
+							itens2[pos].valor, itens2[pos].quantidade);
+					indiceLista++;
+				}
+
+				bitmask >>= 1;
+				pos--;
+
+			}
+
+			listaSubconjuntos.add(listaAux);
+			indiceLista = 0;
+		}
+		return listaSubconjuntos;
 	}
 
 }
